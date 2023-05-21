e5548a9420d256cdecf48e324a6d7ab2bf5d71b9
Fedulov Anton, P <live.stone.anton@gmail.com>
2023-05-22 01:22:24 +0500

    fix: hydration error

--- src/components/markdown/index.tsx
+++ src/components/markdown/index.tsx
@@ -1,8 +1,11 @@
 'use client';
 
+import dynamic from 'next/dynamic';
 import { useTheme } from 'next-themes';
 import { useMemo } from 'react';
 import ReactMarkdown from 'react-markdown';
+
+import { useSsr } from '@/lib/hooks';
 
 import { getMarkdownComponents } from './components';
 import './markdown.css';
@@ -13,12 +16,21 @@ interface MarkdownProps {
   components?: MarkdownComponents;
 }
 
+// stupid hack to get rid of hydration error
+// @ts-ignore
+const ServerReactMarkdown = dynamic(() => import('react-markdown').then((m) => m.default), {});
+
 export function Markdown(props: MarkdownProps) {
   const { children } = props;
   const { resolvedTheme: theme } = useTheme();
+  const { isMounted } = useSsr();
 
   const components = useMemo(() => getMarkdownComponents(theme ?? null), [theme]);
-  console.log({ components });
 
-  return <ReactMarkdown components={components}>{children}</ReactMarkdown>;
+  if (isMounted) {
+    return <ReactMarkdown components={components}>{children}</ReactMarkdown>;
+  }
+  return (
+    <ServerReactMarkdown components={getMarkdownComponents(null)}>{children}</ServerReactMarkdown>
+  );
 }
