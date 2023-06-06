0b14c127541c47228cf9ad6021b000ae55a84d7b
Fedulov Anton, P <live.stone.anton@gmail.com>
2023-06-07 00:15:45 +0500

    feat: added shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,3 @@
-const plugin = require('tailwindcss/plugin');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ['class', '[data-theme="dark"]'],
@@ -29,6 +27,39 @@ module.exports = {
       'lighter-purple': 'var(--lighter-purple)',
       'darker-gray': 'var(--darker-gray)',
       gray: 'var(--gray)',
+      border: 'var(--border)',
+      input: 'var(--input)',
+      ring: 'var(--ring)',
+      background: 'var(--background)',
+      foreground: 'var(--foreground)',
+      primary: {
+        DEFAULT: 'var(--primary)',
+        foreground: 'var(--primary-foreground)',
+      },
+      secondary: {
+        DEFAULT: 'var(--secondary)',
+        foreground: 'var(--secondary-foreground)',
+      },
+      destructive: {
+        DEFAULT: 'var(--destructive)',
+        foreground: 'var(--destructive-foreground)',
+      },
+      muted: {
+        DEFAULT: 'var(--muted)',
+        foreground: 'var(--muted-foreground)',
+      },
+      accent: {
+        DEFAULT: 'var(--accent)',
+        foreground: 'var(--accent-foreground)',
+      },
+      popover: {
+        DEFAULT: 'var(--popover)',
+        foreground: 'var(--popover-foreground)',
+      },
+      card: {
+        DEFAULT: 'var(--card)',
+        foreground: 'var(--card-foreground)',
+      },
     },
     fontSize: {
       xs: '0.75rem',
@@ -51,7 +82,21 @@ module.exports = {
       sm: '0px -0.25px 1px 0.75px rgba(var(--shadow-color-border), 0.25), 0px 8px 20px rgba(var(--shadow-color-main), 0.23), 0px 2px 9px rgba(var(--shadow-color-main), 0.14), 0px 1px 3px rgba(var(--shadow-color-main), 0.09)',
       md: '0px -0.5px 1px 1.5px rgba(var(--shadow-color-border), 0.25), 0px 16px 80px rgba(var(--shadow-color-main), 0.23), 0px 4px 18px rgba(var(--shadow-color-main), 0.14), 0px 1px 6px rgba(var(--shadow-color-main), 0.09)',
     },
+    keyframes: {
+      'accordion-down': {
+        from: { height: 0 },
+        to: { height: 'var(--radix-accordion-content-height)' },
+      },
+      'accordion-up': {
+        from: { height: 'var(--radix-accordion-content-height)' },
+        to: { height: 0 },
+      },
+    },
+    animation: {
+      'accordion-down': 'accordion-down 0.2s ease-out',
+      'accordion-up': 'accordion-up 0.2s ease-out',
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: [require('tailwindcss-animate')],
 };
